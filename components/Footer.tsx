6a64d9cfcb0bc3511fd606db8e634479b7b430dc
SPT-NODE <115471452+sipalingtestnet@users.noreply.github.com>
2023-12-07 04:26:50 +0800

    dsds

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { FaGithub, FaLinkedinIn, FaTwitter, FaYoutube } from 'react-icons/fa'
+import React from 'react';
 
 const Footer = () => {
   return (
-  
-  )
-}
+    <footer className="text-center">
+      <p className="text-gray-600">© 2023 Your Website. All rights reserved.</p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
